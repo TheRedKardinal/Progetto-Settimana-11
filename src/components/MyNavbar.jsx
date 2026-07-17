37e851effa492ccc53f98566709e70ef8a415df5
TheRedKardinal <claudio_giasi@icloud.com>
2026-07-17 12:20:22 +0200

    Navbar stilizzata meglio

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,17 +1,23 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 function MyNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="md">
       <Container fluid>
-        <Navbar.Brand href="#">LOGO</Navbar.Brand>
-        <Navbar.Toggle aria-controls="header-navbar" />
-        <Navbar.Collapse id="header-navbar">
-          <Nav className="ms-auto me-5 d-flex justify-content-between gap-5">
+        <Navbar.Brand href="#" className="logo">
+          CodeMusic <i class="bi bi-vinyl"></i>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="hamburger-navbar" />
+        <Navbar.Collapse id="hamburger-navbar">
+          <Nav className="ms-auto me-5 d-flex justify-content-between gap-4">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">Novità</Nav.Link>
             <Nav.Link href="#">Radio</Nav.Link>
             <Nav.Link href="#">La tua libreria</Nav.Link>
+          </Nav>
+          <Nav className="d-flex gap-2">
+            <Button>Registrati</Button>
+            <Button>Login</Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
